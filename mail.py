3fc6251bc620db0d67a0a79fddba20d6617b654e
superducktoes <nroy@TNS3086L.local>
2016-10-27 16:23:55 -0400

    alert class
    added alert class. build method for sending emails with subject and
    correct to/from

--- mail.py
+++ mail.py
@@ -2,13 +2,32 @@ import requests
 import json
 import urllib3
 import smtplib
+from email.mime.text import MIMEText
 
 login = ''
 password = ''
-url = ''
+url = 'https://'
 
 requests.packages.urllib3.disable_warnings()
 
+#alert class. more to be added here. needs to support adding to/from,subject,message
+
+class Alert:
+
+    def __init__(self):
+        self.recipient = ""
+        self.sender = ""
+        self.message = ""
+        
+    def updateRecipient(self,name):
+        self.recipient = name
+
+    def updateSender(self,name):
+        self.sender = name
+
+    def updateMessage(self,message):
+        self.message = message
+        
 def getTokenCookie():
     headers = {'Content-type': 'application/json'}
     sc = requests.post(url+'/rest/token',
@@ -19,32 +38,37 @@ def getTokenCookie():
     token = sc.json()['response']['token']
     login_header = {'X-SecurityCenter':str(token)}
     headers.update(login_header)
-
+    
     return(cookie,headers)
 
+#going to be moved into the alert class
 def sendMail():
-    sender = 'test@tenablesc.com'
-    receiver = {'nroy@tenable.com'}
 
-    message = """From: SecurityCenter <test@sc.com>
-    To: Test User <test@example.com>
-    Subject: Scan Starting
+    message = '''
+    You have a scan that is starting soon!
+    '''
+
+    msg = MIMEText(message,'plain')
+    msg['Subject'] = "scan starting soon"
+    me = 'user@user.com'
+    msg['From'] = me
+    msg['To'] = me
     
-    There is a scan starting in the next half hour
-    """
-
     try:
-        s = smtplib.SMTP('',25)
-        s.sendmail(sender,receiver,message)
+        s = smtplib.SMTP('mailserver.com',25)
+        s.send_message(msg) #sender,receiver,message
         print("sent email")
-    except:
+    except Exception as e:
+        print(e)
         print("There was an error sending the email")
 
+        
 if __name__=='__main__':
     print("Getting logged into SecurityCenter")
-
+    
     cookie,headers = getTokenCookie()
     print("got cookies and header information")
     
     sendMail()
     
+            
